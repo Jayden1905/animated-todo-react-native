eb40a3f987356b67e4d89e5a80b5584028a14f10
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-01-29 16:22:38 +0800

    adding new item and deleting items done

--- src/screens/main-screen.tsx
+++ src/screens/main-screen.tsx
@@ -1,38 +1,101 @@
 import React, { useCallback, useState } from 'react'
-import { Box, Center, VStack } from 'native-base'
+import { Center, VStack, Fab, Icon, useColorModeValue } from 'native-base'
+import { AntDesign } from '@expo/vector-icons'
 import ThemeToggle from '../components/theme-toggle'
-import TaskItem from '../components/task-item'
+import TaskList from '../components/task-list'
+import shortid from 'shortid'
+import { TaskItemData } from '../components/task-list'
+
+const initialData = [
+  {
+    id: shortid.generate(),
+    subject: 'Buy movie tickets for Friday',
+    done: false,
+  },
+  {
+    id: shortid.generate(),
+    subject: 'Make a reservation for dinner',
+    done: false,
+  },
+]
 
 export default function MainScreen() {
-  const [checked, setChecked] = useState<boolean>(false)
-  const [subject, setSubject] = useState<string>('Task Item')
-  const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [data, setData] = useState(initialData)
+  const [editingItemId, setEditingItemId] = useState<string | null>(null)
 
-  const handleCheckboxPress = useCallback(() => {
-    setChecked((prev) => !prev)
+  const handleToggleTaskItem = useCallback((item: TaskItemData) => {
+    setData((prevData) => {
+      const newData = [...prevData]
+      const index = prevData.indexOf(item)
+      newData[index] = {
+        ...item,
+        done: !item.done,
+      }
+      return newData
+    })
+  }, [])
+
+  const handleChangeTaskItemSubject = useCallback(
+    (item: TaskItemData, subject: string) => {
+      setData((prevData) => {
+        const newData = [...prevData]
+        const index = prevData.indexOf(item)
+        newData[index] = {
+          ...item,
+          subject,
+        }
+        return newData
+      })
+    },
+    []
+  )
+
+  const handleFinishEditingTaskItem = useCallback(() => {
+    setEditingItemId(null)
+  }, [])
+
+  const handlePressTaskItemLabel = useCallback((item: any) => {
+    setEditingItemId(item.id)
+  }, [])
+
+  const handleRemoveItem = useCallback((item: TaskItemData) => {
+    setData((prevData) => {
+      const newData = prevData.filter((i) => i !== item)
+      return newData
+    })
   }, [])
 
   return (
     <Center
       _dark={{ bg: 'blueGray.900' }}
       _light={{ bg: 'blueGray.50' }}
-      px={4}
       flex={1}
     >
       <VStack space={5} alignItems='center' w='full'>
-        <Box w={'full'}>
-          <TaskItem
-            isEditing={isEditing}
-            isDone={checked}
-            subject={subject}
-            onPressLabel={() => setIsEditing(true)}
-            handleCheckboxPress={handleCheckboxPress}
-            onChangeSubject={setSubject}
-            onFinishEditing={() => setIsEditing(false)}
-          />
-        </Box>
+        <TaskList
+          data={data}
+          onToggleItem={handleToggleTaskItem}
+          onChangeSubject={handleChangeTaskItemSubject}
+          onFinishEditing={handleFinishEditingTaskItem}
+          onPressLabel={handlePressTaskItemLabel}
+          onRemoveItem={handleRemoveItem}
+          eidtingItemId={editingItemId}
+        />
         <ThemeToggle />
       </VStack>
+      <Fab
+        position='absolute'
+        renderInPortal={false}
+        size='sm'
+        icon={<Icon color='white' as={<AntDesign name='plus' />} />}
+        colorScheme={useColorModeValue('blue', 'darkBlue')}
+        bg={useColorModeValue('blue.500', 'blue.400')}
+        onPress={() => {
+          const id = shortid.generate()
+          setData([{ id, subject: '', done: false }, ...data])
+          setEditingItemId(id)
+        }}
+      />
     </Center>
   )
 }
